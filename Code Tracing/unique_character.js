462a7054c10fb22a53679fab2f72cfa0f371d9f7
Metrosmoke-Jailen <vhzdy4qyj2@privaterelay.appleid.com>
2026-09-23 15:17:49 -0700

    Update unique_character.js

--- Code Tracing/unique_character.js
+++ Code Tracing/unique_character.js
@@ -17,3 +17,14 @@ function firstUniqChar(s) {
 console.log(firstUniqChar("leetcode"));
 console.log(firstUniqChar("loveleetcode"));
 console.log(firstUniqChar("aabb"));
+
+function firstUniqChar(s) {
+  const counts = {};
+  for (const char of s) {
+    counts[char] = (counts[char] || 0) + 1;
+  }
+  for (let i = 0; i < s.length; i++) {
+    if (counts[s[i] === 1]) return i;
+  }
+  return -1;
+}
