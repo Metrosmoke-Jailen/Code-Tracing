0883f543181fcb5558e4aae6778a5a356830b6b8
Metrosmoke-Jailen <vhzdy4qyj2@privaterelay.appleid.com>
2026-09-23 15:17:27 -0700

    Update two_sums.js

--- Code Tracing/two_sums.js
+++ Code Tracing/two_sums.js
@@ -15,3 +15,15 @@ function twoSum(nums, target) {
 }
 
 console.log(twoSum([3, 2, 4], 6));
+
+function twoSum(nums, target) {
+  const seen = {};
+  for (let i = 0; i < nums.length; i++) {
+    seen[nums[i]] = i;
+    const complement = target - nums[i];
+    if (seen[complement] !== undefined) {
+      return [seen[complement], i];
+    }
+  }
+  return [];
+}
