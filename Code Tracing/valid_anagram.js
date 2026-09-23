e21015c09b72306053d4f5bdfa4716318e6a311e
Metrosmoke-Jailen <vhzdy4qyj2@privaterelay.appleid.com>
2026-09-23 15:18:16 -0700

    Update valid_anagram.js

--- Code Tracing/valid_anagram.js
+++ Code Tracing/valid_anagram.js
@@ -18,3 +18,13 @@ function isAnagram(s, t) {
 console.log(isAnagram("ab", "abc"));
 console.log(isAnagram("listen", "silent"));
 console.log(isAnagram("hello", "world"));
+
+function isAnagram(s, t) {
+  const counts = {};
+  for (const char of s) counts[char] = (counts[char] || 0) + 1;
+  for (const char of t) counts[char] = (counts[char] || 0) - 1;
+  for (const key in counts) {
+    if (counts[key] > 0) return false;
+  }
+  return true;
+}
