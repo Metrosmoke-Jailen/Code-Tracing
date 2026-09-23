7597ebd029d81fa7cbbbded51efc4b2df0c22eba
Metrosmoke-Jailen <vhzdy4qyj2@privaterelay.appleid.com>
2026-09-23 15:15:15 -0700

    Add files via upload

---
+++ Code Tracing/valid_anagram.js
@@ -0,0 +1,20 @@
+function isAnagram(s, t) {
+  const counts = {};
+
+  for (const char of s)
+    counts[char] = (counts[char] || 0) + 1;
+
+  for (const char of t)
+    counts[char] = (counts[char] || 0) - 1;
+
+  for (const key in counts) {
+    if (counts[key] > 0)
+      return false;
+  }
+
+  return true;
+}
+
+console.log(isAnagram("ab", "abc"));
+console.log(isAnagram("listen", "silent"));
+console.log(isAnagram("hello", "world"));
